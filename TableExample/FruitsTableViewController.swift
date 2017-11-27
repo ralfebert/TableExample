d873c792d0483a4dd795c5025bd1f2152321e867
Ralf Ebert <ralf.ebert@gmail.com>
2017-11-27 12:04:08 +0100

    custom cells

--- TableExample/FruitsTableViewController.swift
+++ TableExample/FruitsTableViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+class FruitTableViewCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var fruitImageView: UIImageView!
+}
+
 class FruitsTableViewController: UITableViewController {
 
     var fruits = ["Apple", "Apricot", "Banana", "Blueberry", "Cantaloupe", "Cherry", "Clementine", "Coconut", "Cranberry", "Fig", "Grape", "Grapefruit", "Kiwi fruit", "Lemon", "Lime", "Lychee", "Mandarine", "Mango", "Melon", "Nectarine", "Olive", "Orange", "Papaya", "Peach", "Pear", "Pineapple", "Raspberry", "Strawberry"]
@@ -23,13 +28,11 @@ class FruitsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath) as! FruitTableViewCell
 
         let fruitName = fruits[indexPath.row]
-        cell.textLabel?.text = fruitName
-        cell.detailTextLabel?.text = "Delicious!"
-        cell.imageView?.image = UIImage(named: fruitName)
-        
+        cell.label?.text = fruitName
+        cell.fruitImageView?.image = UIImage(named: fruitName)
         return cell
     }
 
